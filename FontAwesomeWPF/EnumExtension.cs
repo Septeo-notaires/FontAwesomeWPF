0339af722dbd95e2e228bb6943ce069396ae33b5
Damien MODIN <damien.modin@septeo.com>
2025-05-30 16:12:13 +0200

    add projects

---
+++ FontAwesomeWPF/EnumExtension.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace FontAwesomeWPF
+{
+    public static class EnumExtension
+    {
+        /// <summary>
+        /// Recupere la valeur (int) de l'enum
+        /// </summary>
+        /// <param name="value">Methode d'extension d'une enumération</param>
+        public static int ToInt(this Enum value) => Convert.ToInt32(value);
+
+        /// <summary>
+        /// Methode d'extension permettant de récupérer la valeur de l'attribut "Description"
+        /// e.g. [Description("Day of week. Sunday")]
+        /// </summary>
+        /// <param name="value">Methode d'extension d'une enumération</param>
+        /// <returns>Valeur de l'attribut Description</returns>
+        public static string ToDescription(this Enum value) => value.GetAttributeValue<DescriptionAttribute, string>(attr => attr.Description);
+
+        /// <summary>
+
+        /// <summary>
+        /// Cette methode d'extension permet de récupèrer l'attribut d'une énumération
+        /// </summary>
+        /// <typeparam name="T">Type de l'attribut</typeparam>
+        /// <returns>Attribut concerné</returns>
+        private static T GetAttribute<T>(this Enum value) where T : Attribute
+        {
+            var memberInfo = value.GetType().GetMember(value.ToString()).FirstOrDefault();
+            return memberInfo?.GetCustomAttributes(typeof(T), false).FirstOrDefault() as T;
+        }
+
+        /// <summary>
+        /// Cette methode d'extension permet de récupèrer la valeur d'un attribut
+        /// </summary>
+        /// <typeparam name="TAttribute">Type de l'attribut</typeparam>
+        /// <typeparam name="TResult">Type du resultat attendu</typeparam>
+        /// <param name="selector">Selecteur de la propriete de l'attribut a recuperer</param>
+        /// <returns>Valeur de l'attribut a recuperer</returns>
+        private static TResult GetAttributeValue<TAttribute, TResult>(this Enum value, Func<TAttribute, TResult> selector) where TAttribute : Attribute
+        {
+            var attribute = value.GetAttribute<TAttribute>();
+            return attribute != null ? selector(attribute) : default;
+        }
+    }
+
+    public enum EnumAttributeFrom
+    {
+        Name,
+        DisplayName,
+        ShortName,
+        Int,
+        Code
+    }
+}
---
+++ FontAwesomeWPF/FontAwesomeIcon.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace FontAwesomeWPF
+{
+    /// <summary>
+    /// Interaction logic for FontAwesomeIcon.xaml
+    /// </summary>
+    public partial class FontAwesomeIcon : UserControl
+    {
+        public FontAwesomeIcon()
+        {
+            InitializeComponent();
+            FontFamily faFont = Application.Current.Resources[FontAwesomeIconStyle.Regular.ToDescription()] as FontFamily;
+            FontFamily = faFont;
+        }
+
+        public static readonly DependencyProperty IconNameProperty = DependencyProperty.Register(
+            "IconName",
+            typeof(FontAwesomeIconName),
+            typeof(FontAwesomeIcon),
+            new PropertyMetadata(FontAwesomeIconName.House, OnIconNameChanged)
+        );
+
+        public FontAwesomeIconName IconName
+        {
+            get => (FontAwesomeIconName)GetValue(IconNameProperty);
+            set => SetValue(IconNameProperty, value);
+        }
+
+        private static void OnIconNameChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is FontAwesomeIcon iconControl && e.NewValue is FontAwesomeIconName iconName)
+            {
+                if (FontAwesomeIconMap.IconMap.TryGetValue(iconName, out string unicode))
+                {
+                    iconControl.Icon = unicode;
+                }
+            }
+        }
+
+        public static readonly DependencyProperty IconStyleProperty = DependencyProperty.Register(
+            "IconStyle",
+            typeof(FontAwesomeIconStyle),
+            typeof(FontAwesomeIcon),
+            new PropertyMetadata(FontAwesomeIconStyle.Regular, OnIconStyleChanged)
+        );
+
+        public FontAwesomeIconStyle IconStyle
+        {
+            get => (FontAwesomeIconStyle)GetValue(IconStyleProperty);
+            set => SetValue(IconStyleProperty, value);
+        }
+
+        private static void OnIconStyleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is FontAwesomeIcon iconControl && e.NewValue is FontAwesomeIconStyle iconStyle)
+            {
+                FontFamily faFont = Application.Current.Resources[iconStyle.ToDescription()] as FontFamily;
+                iconControl.FontFamily = faFont;
+            }
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(string), typeof(FontAwesomeIcon), new PropertyMetadata("\uF015"));
+
+        public string Icon
+        {
+            get => (string)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        public static readonly DependencyProperty IconColorProperty =
+            DependencyProperty.Register("IconColor", typeof(Brush), typeof(FontAwesomeIcon), new PropertyMetadata(Brushes.Black));
+
+        public Brush IconColor
+        {
+            get => (Brush)GetValue(IconColorProperty);
+            set => SetValue(IconColorProperty, value);
+        }
+
+        public static readonly DependencyProperty IconSizeProperty =
+            DependencyProperty.Register("IconSize", typeof(double), typeof(FontAwesomeIcon), new PropertyMetadata(24.0));
+
+        public double IconSize
+        {
+            get => (double)GetValue(IconSizeProperty);
+            set => SetValue(IconSizeProperty, value);
+        }
+    }
+
+    public enum FontAwesomeIconStyle
+    {
+        [Description("FontAwesomeRegular")]
+        Regular,
+
+        [Description("FontAwesomeSolid")]
+        Solid,
+
+        [Description("FontAwesomeBrands")]
+        Brands
+        //Light,
+    }
+}
